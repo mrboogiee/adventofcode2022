3df2fd1a393f73eb80a4d59b3dc4435dcd8d16fc
Johan Bogema <johan.bogema@on2it.net>
2022-12-01 07:38:46 +0100

    why sort by yourself if there is a sort function...

--- day1b/main.go
+++ day1b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sort"
 	"strconv"
 )
 
@@ -10,7 +11,6 @@ func main() {
 	lines, _ := readLines("input.txt")
 	var elves []int
 	currentCarrying, elveIndex := 0, 0
-	bigThreeCarriers := []int{0, 0, 0} // big three carriers of calories
 	for i, line := range lines {
 		if line != "" { // non-empty line so elve is carrying something
 			intValue, _ := strconv.Atoi(line)
@@ -24,18 +24,6 @@ func main() {
 			elves = append(elves, currentCarrying)
 		}
 	}
-	for _, elve := range elves {
-		if elve > bigThreeCarriers[0] { // if the elve is bigger than the number one, shift all one down
-			bigThreeCarriers[2] = bigThreeCarriers[1]
-			bigThreeCarriers[1] = bigThreeCarriers[0]
-			bigThreeCarriers[0] = elve
-		} else if elve > bigThreeCarriers[1] { // if the elve is bigger than the number 2, shift 2 and 3 down
-			bigThreeCarriers[2] = bigThreeCarriers[1]
-			bigThreeCarriers[1] = elve
-			bigThreeCarriers[1] = elve
-		} else if elve > bigThreeCarriers[2] { // if the elve is bigger than the number 3, replace number 3
-			bigThreeCarriers[2] = elve
-		}
-	}
-	log.Println("Total calories carried by the top 3: ", bigThreeCarriers[0]+bigThreeCarriers[1]+bigThreeCarriers[2])
+	sort.Ints(elves)
+	log.Println("Total calories carried by the top 3: ", elves[len(elves)-1]+elves[len(elves)-2]+elves[len(elves)-3])
 }
